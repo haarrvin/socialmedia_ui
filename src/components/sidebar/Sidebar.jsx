3f189e558cbf0a3a855652988e89a0aec1e8fd21
HAARRVIN VATHARAJU <40566954+haarrvin@users.noreply.github.com>
2023-11-17 09:30:07 +0800

    Completed sidebar component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,63 @@
 import "./sidebar.css"
+import { Bookmark, Event, PlayCircleFilledOutlined, RssFeed, School, WorkOutline, Chat, Group, HelpOutline  } from "@mui/icons-material" 
 
 export default function Sidebar() {
   return (
     <div className="sidebar">
-      Sidebar
+        <div className="sidebarWrapper">
+            <ul className="sidebarList">
+              <li className="sidebarListItem">
+                <RssFeed className="sidebarIcon" />
+                <span className="sidebarListItemText">Feed</span>
+              </li>
+              <li className="sidebarListItem">
+                <Chat className="sidebarIcon" />
+                <span className="sidebarListItemText">Chats</span>
+              </li>
+            <li className="sidebarListItem">
+              <PlayCircleFilledOutlined className="sidebarIcon" />
+              <span className="sidebarListItemText">Videos</span>
+            </li>
+            <li className="sidebarListItem">
+              <Group className="sidebarIcon" />
+              <span className="sidebarListItemText">Groups</span>
+            </li>
+            <li className="sidebarListItem">
+              <Bookmark className="sidebarIcon" />
+              <span className="sidebarListItemText">Bookmarks</span>
+            </li>
+            <li className="sidebarListItem">
+              <HelpOutline className="sidebarIcon" />
+              <span className="sidebarListItemText">Questions</span>
+            </li>
+            <li className="sidebarListItem">
+              <WorkOutline className="sidebarIcon" />
+              <span className="sidebarListItemText">Jobs</span>
+            </li>
+            <li className="sidebarListItem">
+              <Event className="sidebarIcon" />
+              <span className="sidebarListItemText">Events</span>
+            </li>
+            <li className="sidebarListItem">
+              <School className="sidebarIcon" />
+              <span className="sidebarListItemText">Courses</span>
+            </li>
+          </ul>
+          <button className="sidebarButton">Show More</button>
+          <hr className="sidebarHr"/>
+          <ul className="sidebarFriendList">
+            <li className="sidebarFriend">
+              <img className="sidebarFriendImg" src="/assets/people/u2.jpg" alt=""/>
+              < span className="sidebarFriendName">Elon Musk</span>            
+            </li>
+
+
+            <li className="sidebarFriend">
+              <img className="sidebarFriendImg" src="/assets/people/u3.jpg" alt=""/>
+              < span className="sidebarFriendName">Joe Carbstrong</span>            
+            </li>
+          </ul>
+        </div>
       </div>
   )
 }
- 
