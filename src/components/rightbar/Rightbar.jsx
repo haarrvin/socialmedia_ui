ffd3dbe4d075332d1e9d5f44725d74274e584aa0
Haarrvin <haarrvin.gvraj@gmail.com>
2023-12-24 22:18:33 +0800

    Added right bar with birthday

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,12 @@ import "./rightbar.css";
 export default function Rightbar() {
   return (
     <div className="rightbar">
-      Rightbar
+      <div className="birthdayContainer">
+        <img className="birthdayImg" src="assets/gift.png" alt="" />
+        <span className="">
+          <b>Pola Foster</b> and <b>18 other friends</b> have a birthday tpday
+        </span>
+      </div>
     </div>
   )
 }
